0fd979deca388927e34214ec3e4af3d046da201a
bg.sim <bg.sim@snu.ac.kr>
2023-12-08 13:52:26 +0900

    upload button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Mainplot from "./components/Mainplot";
+import Button from '@material-ui/core/Button';
 
 import "./App.css";
 
 function App() {
   const mainref = Mainplot();
-  
+
+  function uploadbuttonclick(filehandle)
+  {
+    console.log(filehandle.target.value)
+  }
+
   return (
     <div className="App">
       <AppBar position="static" sx={{width: "100%", height: 80, backgroundColor: "#060017"}}>
@@ -26,11 +32,23 @@ function App() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: 2, fontSize: "2rem", lineHeight: 5 }}>
             VisRPW
           </Typography>
+          <input
+            type="file"
+            accept=".json,.bin"
+            style={{ display: 'none' }}
+            id="contained-button-file"
+            onChange={uploadbuttonclick}
+          />
+          <label htmlFor="contained-button-file">
+            <Button variant="contained" color="primary" component="span">
+              Upload
+            </Button>
+          </label>
         </Toolbar>
-      </AppBar>  
-      <div style={{ width: "100%"}}>    
-        <Mainplot />  
-      </div> 
+      </AppBar>
+      <div style={{ width: "100%"}}>
+        <Mainplot />
+      </div>
     </div>
   );
 }
